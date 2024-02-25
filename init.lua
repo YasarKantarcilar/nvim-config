411af419c51de16f8345afb9601454f351d878a6
YasarKantarcilar <kantarcilaryasar@gmail.com>
2024-02-25 12:52:52 +0300

    update key bindings

--- init.lua
+++ init.lua
@@ -1,32 +1,19 @@
--- Check if vim-plug is installed
 require("config.lazy")
-local plug_path = vim.fn.stdpath('data')..'/site/autoload/plug.vim'
-if vim.fn.empty(vim.fn.glob(plug_path)) >  0 then
-  -- Install vim-plug
-  vim.cmd('!curl -fLo '..plug_path..' --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
-  -- Set up an autocmd to run PlugInstall and reload the configuration file on startup
-  vim.cmd([[
-    autocmd VimEnter * PlugInstall | source $MYVIMRC
-  ]])
-else
-  -- If vim-plug is already installed, load the plugins
-  vim.cmd([[
-    call plug#begin('~/AppData/Local/nvim/plugged')
-    Plug 'mg979/vim-visual-multi', {'branch': 'master'}
-    Plug 'andweeb/presence.nvim'
-    Plug 'windwp/nvim-ts-autotag', {'do': ':TSUpdate'}
-    call plug#end()
-  ]])
 
-  -- Set up an autocmd to check if plugins are not installed and install them
-  vim.cmd([[
-    autocmd VimEnter * if len(filter(values(g:plugs), '!isdirectory(v:val.dir)')) | PlugInstall --sync | q | endif
-  ]])
+vim.api.nvim_set_keymap(
+  "n",
+  "<Leader>har",
+  [[:lua require('harpoon.ui').toggle_quick_menu()<CR>]],
+  { noremap = true, silent = true }
+)
 
-  -- Configure nvim-ts-autotag
-  require('nvim-treesitter.configs').setup {
-    autotag = {
-      enable = true,
-    },
-  }
-end
+-- Add current file to Harpoon bookmarks
+vim.api.nvim_set_keymap(
+  "n",
+  "<Leader>haa",
+  [[:lua require("harpoon.mark").add_file()<CR>]],
+  { noremap = true, silent = true }
+)
+
+-- Remove Harpoon marks
+vim.api.nvim_set_keymap("n", "<Leader>had", [[:HarpoonRm<Space>]], { noremap = true, silent = true })
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,12 +16,15 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
+    -- Add Harpoon
+    { "nvim-lua/plenary.nvim" },
+    { "ThePrimeagen/harpoon" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
     -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- It's recommended to leave version=false for now, since a lot of plugins that support versioning
     -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
